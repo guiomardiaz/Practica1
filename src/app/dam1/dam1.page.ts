98b7b71a96080de7123cdbc8f10b4a461cc304e1
guiomardiaz <guiomir2507@gmail.com>
2022-10-06 18:26:44 +0200

    COMMMITTTTTTTTT

--- src/app/dam1/dam1.page.ts
+++ src/app/dam1/dam1.page.ts
@@ -8,6 +8,42 @@ import { Component, OnInit } from '@angular/core';
 export class Dam1Page implements OnInit {
 
   constructor() { }
+  asignaturasDAM1 = [
+    {
+      icono: "people-circle-outline",
+      nombre: "Servicios y Procesos",
+      checkbox: ""
+
+    },
+    {
+      icono: "finger-print-outline",
+      nombre: "Desarrollo de Interfaces",
+      checkbox: ""
+    },
+    {
+      icono: "bag-remove-outline",
+      nombre: "Sistemas de Gestion Empresarial",
+      checkbox: ""
+      
+
+    },
+    {
+      icono: "analytics-outline",
+      nombre: "Acceso a Datos",
+      checkbox: ""
+    },
+    {
+      icono: "aperture-outline",
+      nombre: "Programacion Multimedia",
+      checkbox: ""
+    },
+    {
+      icono: "airplane-outline",
+      nombre: "Empresas",
+      checkbox: ""
+    }
+    
+  ]
 
   ngOnInit() {
   }
