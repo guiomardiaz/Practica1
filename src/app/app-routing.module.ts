b5e2c53488862ac5e5108c2d291f88c65458f070
camardaa <carmelolosa@gmail.com>
2022-10-06 18:21:26 +0200

    munchismoxd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,18 +24,11 @@ const routes: Routes = [
     loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
   },
   {
-    path: 'dam2',
-    loadChildren: () => import('./dam2/dam2.module').then( m => m.Dam2PageModule)
-  },
-
-  {
-    path: 'dam1',
-    loadChildren: () => import('./dam1/dam1.module').then( m => m.Dam1PageModule)
-  },
-  {
     path: 'componentes',
     loadChildren: () => import('./componentes/componentes.module').then( m => m.ComponentesModule)
-  }
+  },
+  
+
 ];
 
 @NgModule({
--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -6,8 +6,25 @@ import { TabsPage } from './tabs.page';
 const routes: Routes = [
   {
     path: '',
-    component: TabsPage
+    component: TabsPage,
+    children: [
+      {
+        path: 'fct',
+        loadChildren: () => import('./../fct/fct.module').then( m => m.FctPageModule)
+      },
+      {
+        path: 'dam2',
+        loadChildren: () => import('./../dam2/dam2.module').then( m => m.Dam2PageModule)
+      },
+    
+      {
+        path: 'dam1',
+        loadChildren: () => import('./../dam1/dam1.module').then( m => m.Dam1PageModule)
+      },
+
+    ]
   }
+ 
 ];
 
 @NgModule({
